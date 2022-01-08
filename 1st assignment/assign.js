9b59d916051b3a6d98f10b930a2536ad848d4875
Meherazhossain91 <itsonlyopu@gmail.com>
2022-01-08 23:31:16 +0600

    assignment 1

---
+++ 1st assignment/assign.js
@@ -0,0 +1,50 @@
+
+// problem 1
+console.log('Md Meheraz Hossain');
+console.dir('daffodil university ')
+
+// problem 2
+
+var name = prompt('name');
+var skill = prompt('skill');
+
+console.log('My name is '+name+' hossain and i am '+skill+' developer');
+
+// problem 3 
+
+var math = prompt('math')
+var physics = prompt('physics')
+var biology = prompt('biology')
+var english = prompt('english')
+var bangla = prompt('bangla')
+var statistics = prompt('statistics')
+
+
+console.log('math mark: '+math+' physics mark: '+physics+' biology mark: '+biology+' english mark: '+english+' bangla mark:  '+bangla+' statistics mark: '+statistics+'');
+
+// problem 4
+
+
+ var name = prompt('Enter name(jamai ar name )');
+ var name2 = prompt('Enter name(bow ar name)');
+ var confirms = confirm(" jonab apni ki razi");
+ var confirms2 = confirm(' mts apni ki razi');
+ var confirms3 = confirm(' jonab 10 lakh tkr dan mohor a  apni ki biyete  razi');
+ var confirms4 = confirm(' mts  10 lakh tkr dan mohor a  apni ki biyete  razi');
+
+
+ console.log('jamai ar name: '+name+'');
+ console.log('bow ar name: '+name2+'');
+ console.log('jamai ar motamot: '+confirms+'');
+ console.log('bow ar motamot: '+confirms2+'');
+ console.log('Dan mohor diye jamai  ar motamot: '+confirms3+'');
+ console.log('Dan mohor diye bow  ar  motamot: '+confirms4+'');
+
+
+  
+
+
+
+
+
+
